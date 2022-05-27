b6439a6b96fc897bf96e10e79171b216e092b8ad
Danielle Dempsey <ddempsey@perennia.ca>
2022-05-27 14:00:38 -0300

    fix: deleted the knit_exit() from bottom of file so all reports can be generated from mapply

--- generate_adcp_reports.R
+++ generate_adcp_reports.R
@@ -9,38 +9,30 @@ library(here)
 library(readxl)
 
 
-
-# SECTION 1: Specify counties ---------------------------------------------
+# SECTION 1: Specify deployments ---------------------------------------------
 
 report <- here("ADCP_Report.Rmd")
 
-tracker <- read_xlsx("Z:/Coastal Monitoring Program/ADCP/Side Lobe Trimmed/Reports/ADCP_Report_Tracker.xlsx")
-tracker <- tracker %>% filter(is.na(`2022 Report (sidelobe trimmed)`))
+tracker <- read_xlsx("Y:/Coastal Monitoring Program/ADCP/Side Lobe Trimmed/Reports/ADCP_Report_Tracker.xlsx")
+tracker <- tracker %>%
+  filter(is.na(`2022 Report (sidelobe trimmed)`))
 
-station <- tracker$Open_Data_Station[1:4]
-depl_date <- tracker$Depl_Date[1:4]
-deployment <- data.frame(station, depl_date)
+depl_date <- tracker$Depl_Date[1:2]
+station <- tracker$Open_Data_Station[1:2]
+
 
 # SECTION 2: GENERATE REPORTS --------------------------------------------------------
 
-# sapply(1:4, function(x) {
-#
-#   rmarkdown::render(
-#     input = report,
-#     output_file = paste0(deployment$depl_date[x], "_", deployment$station[x], "_ADCP Report.docx"),
-#     params = list(station = deployment$station[x],
-#                   depl_date = deployment$depl_date[x]))
-#
-# })
+mapply(function(x, y) {
 
-library(purrr)
-
-purrr::walk(
-  .x = 1:4,
-  ~ rmarkdown::render(
+  rmarkdown::render(
     input = report,
-    output_file = paste0(deployment$depl_date[.x], "_", deployment$station[.x], "_ADCP Report.docx"),
-    params = list(station = deployment$station[.x],
-                  depl_date = deployment$depl_date[.x]))
-)
+    output_file = paste0(
+      x, "_", y, "_ADCP Report.docx"
+    ),
+    params = list(depl_date = x, station = y),
+  )
+
+}, depl_date, station)
+
 
