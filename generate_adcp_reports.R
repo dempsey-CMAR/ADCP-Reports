17f4ae96462f80ab726660abd6186a2000225b3c
Joseph Barss <jospbarss@gmail.com>
2022-05-30 15:47:50 -0300

    Make a file for notes

--- generate_adcp_reports.R
+++ generate_adcp_reports.R
@@ -18,8 +18,8 @@ tracker <- read_xlsx("Z:/Coastal Monitoring Program/ADCP/Side Lobe Trimmed/Repor
 tracker <- tracker %>%
   filter(is.na(`2022 Report (sidelobe trimmed)`))
 
-depl_date <- tracker$Depl_Date[1:10]
-station <- tracker$Open_Data_Station[1:10]
+depl_date <- tracker$Depl_Date[60:64]
+station <- tracker$Open_Data_Station[60:64]
 
 
 # SECTION 2: GENERATE REPORTS --------------------------------------------------------
